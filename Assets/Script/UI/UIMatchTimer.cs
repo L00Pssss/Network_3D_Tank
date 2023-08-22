c8619c96f0325b767a7fbbba06fdbb988c3cbc54
L00Pssss <ggstyle20@gmail.com>
2023-08-22 22:38:22 +0300

    UpdateTimer

--- Assets/Script/UI/UIMatchTimer.cs
+++ Assets/Script/UI/UIMatchTimer.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -33,4 +34,48 @@ public class UIMatchTimer : MonoBehaviour
         int seconds = Mathf.FloorToInt(time % 60);
         return $"{minutes:00}:{seconds:00}";
     }
+
+
+
+    //    private string FormatTime(float time)
+    //    {
+    //        int minutes = Mathf.FloorToInt(time / 60);
+    //        int seconds;
+
+    //        if (time >= Timer.TimeLeft)
+    //        {
+    //            float remainder = time % 60;
+    //            int roundedSeconds = Mathf.RoundToInt(remainder);
+
+    //            if (roundedSeconds >= UpdateInterval)
+    //            {
+    //                seconds = roundedSeconds;
+    //            }
+    //            else
+    //            {
+    //                seconds = Mathf.CeilToInt(Timer.TimeLeft % 60); // Округляем начальные секунды вверх до ближайшего целого
+    //            }
+    //        }
+    //        else
+    //        {
+    //            seconds = Mathf.FloorToInt(Timer.TimeLeft % 60);
+
+    //            if (seconds == Mathf.FloorToInt(time % 60))
+    //            {
+    //                seconds = Mathf.CeilToInt(time % 60);
+    //            }
+    //        }
+
+    //        return $"{minutes:00}:{seconds:00}";
+    //    }
+
+
+
+    //////
+    //private string FormatTime(float time)
+    //{
+    //    TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+    //    return timeSpan.ToString(@"mm\:ss");
+    //}
+
 }
