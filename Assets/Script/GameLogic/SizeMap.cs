fe8d36f6e7ff0342aeb2cf13282b5be13eb36f27
L00Pssss <ggstyle20@gmail.com>
2023-08-25 14:52:56 +0300

    Add MiniMap

---
+++ Assets/Script/GameLogic/SizeMap.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SizeMap : MonoBehaviour
+{
+    [SerializeField] private Vector2 size;
+    public Vector2 Size
+    {
+        get { return size; }
+
+    }
+
+    public Vector3 GetNormalPositon(Vector3 positon)
+    {
+        return new Vector3(positon.x / size.x, 0, positon.z / size.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(size.x, 0, size.y));
+    }
+}
---
+++ Assets/Script/UI/UIMinimap.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMinimap : MonoBehaviour
+{
+    [SerializeField] private SizeMap sizeMap;
+
+    [SerializeField] private UITankMark tankMarkPrefab;
+
+    [SerializeField] protected Image background;
+
+    private UITankMark[] tankMarks;
+    private Player[] players;
+
+    private void Start()
+    {
+        NetworkSessionManager.Match.MatchStart += OnMatchStart;
+   //     NetworkSessionManager.Match.MatchStart += OnMatchEnd;
+    }
+
+    private void OnDestroy()
+    {
+        if (NetworkSessionManager.Instance != null && NetworkSessionManager.Match != null)
+        {
+            NetworkSessionManager.Match.MatchStart -= OnMatchStart;
+     //       NetworkSessionManager.Match.MatchStart -= OnMatchEnd;
+        }
+    }
+
+    private void OnMatchStart()
+    {
+        players = FindObjectsOfType<Player>();
+
+        tankMarks = new UITankMark[players.Length];
+
+        Debug.Log(tankMarks.Length);
+
+        for (int i = 0; i < tankMarks.Length; i++) 
+        {
+            tankMarks[i] = Instantiate(tankMarkPrefab);
+
+            if (players[i].TeamId == Player.Local.TeamId)
+                tankMarks[i].SetLocalColor();
+            else
+                tankMarks[i].SetOtherColor();
+
+            tankMarks[i].transform.SetParent(background.transform);
+        }
+
+        for (int i = 0; i < tankMarks.Length; i++)
+        {
+            Debug.Log(tankMarks[i].name);
+        }
+    }
+
+    private void OnMatchEnd()
+    {
+        for (int i = 0; i < background.transform.childCount; i++)
+        {
+            Destroy(background.transform.GetChild(i).gameObject);
+        }
+        tankMarks = null; // на выбор. 
+    }
+
+    private void Update()
+    {
+
+        if (tankMarks == null) return;
+
+        for (int i = 0; i < tankMarks.Length; i++)
+        {
+            Vector3 normalPositon = sizeMap.GetNormalPositon(players[i].ActiveVechicle.transform.position);
+
+            Vector3 posInMinimap = new Vector3(normalPositon.x * background.rectTransform.sizeDelta.x * 0.5f,
+                normalPositon.z * background.rectTransform.sizeDelta.y * 0.5f, 0);
+
+            tankMarks[i].transform.position = background.transform.position + posInMinimap;
+        }
+    }
+}
