4cec6517c9dee483f19a6f3abb44804b27cd7a6b
L00Pssss <ggstyle20@gmail.com>
2023-10-01 16:03:52 +0300

    NavMesh Create and configure

--- Assets/Script/AI/AIMovement.cs
+++ Assets/Script/AI/AIMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public static class TransformExtensions
 {
@@ -27,34 +28,103 @@ public class AIMovement : MonoBehaviour
     [SerializeField] private AIRaySensor sensorBackward;
     [SerializeField] private AIRaySensor sensorRight;
     [SerializeField] private AIRaySensor sensorLeft;
-
-    private Vector3 target;
+    [SerializeField] private int stopDistance = 2;
+    [SerializeField] private float pathUpdateRate = 0.33f;
+     
     
+    private Vector3 target;
 
-
-
+    private Vector3 nextPathPoint;
     private Vehicle vehicle;
+    private NavMeshPath path;
+    private int cornerIndex;
+
+    private float timerUpdatePath;
+    
+    private bool hasPath;
+    private bool reachedDestination; 
 
     private void Awake()
     {
+        path = new NavMeshPath();
         vehicle = GetComponent<Vehicle>();
-    }
-
-    private void Start()
-    {
-        
     }
 
     private void Update()
     {
-        target = GameObject.FindGameObjectWithTag("Finish").transform.position;
+        SetDestination(GameObject.FindGameObjectWithTag("Finish").transform.position);
+
+        if (pathUpdateRate > 0)
+        {
+            timerUpdatePath += timerUpdatePath;
+
+            if (timerUpdatePath > pathUpdateRate)
+            {
+                CalculatePath(target);
+                timerUpdatePath = 0;
+            }
+        }
+
+
+        UpdateTarget();
+
         MoveToTarget();
+    }
+
+    public void SetDestination(Vector3 target)
+    {
+        if(this.target == target && hasPath == true) return;
+        
+        this.target = target;
+        
+        CalculatePath(target);
+    }
+
+    public void ResetPath()
+    {
+        hasPath = false;
+        reachedDestination = false;
+    }
+
+    private void UpdateTarget()
+    {
+        if(hasPath == false) return;
+
+        nextPathPoint = path.corners[cornerIndex];
+
+        if (Vector3.Distance(transform.position, nextPathPoint) < stopDistance)
+        {
+            if (path.corners.Length - 1 > cornerIndex)
+            {
+                cornerIndex++;
+                nextPathPoint = path.corners[cornerIndex];
+            }
+            else
+            {
+                hasPath = false;
+                reachedDestination = true;
+            }
+        }
+        
+        for (int i = 0; i < path.corners.Length - 1; i++)
+            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+    }
+
+    private void CalculatePath(Vector3 target)
+    {
+        NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+
+        hasPath = path.corners.Length > 0;
+        reachedDestination = false;
+
+        cornerIndex = 1;
+
     }
     
     private Vector3 GetReferenceMovementDirectionZX()
     {
         var tankPos = vehicle.transform.GetPositionZX();
-        var targetPos = target.GetPositionZX();
+        var targetPos = nextPathPoint.GetPositionZX();
 
         return (targetPos - tankPos).normalized;
     }
@@ -70,6 +140,13 @@ public class AIMovement : MonoBehaviour
     
     private void MoveToTarget()
     {
+        if(nextPathPoint == null) return;
+
+        if (reachedDestination == true)
+        {
+            vehicle.SetTargetControl(new Vector3(0,1,0));
+            return;
+        }
         float turnControl = 0;
         float forwardThrust = 1;
 
@@ -78,8 +155,8 @@ public class AIMovement : MonoBehaviour
 
 
         var forwardSensorState = sensorForward.Raycast();
-        var leftSensorState = sensorForward.Raycast();
-        var rightSensorState = sensorForward.Raycast();
+        var leftSensorState = sensorLeft.Raycast();
+        var rightSensorState = sensorRight.Raycast();
 
         if (forwardSensorState.Item1)
         {
@@ -105,7 +182,7 @@ public class AIMovement : MonoBehaviour
         {
             turnControl = Mathf.Clamp(Vector3.SignedAngle(tankDir, referenceDirection,Vector3.up), -45.0f, 45.0f) / 45.0f;
 
-            const float minSideDistance = 1;
+            const float minSideDistance = 2;
 
             if (leftSensorState is { Item1: true, Item2: < minSideDistance } && turnControl < 0)
                 turnControl = -turnControl;
